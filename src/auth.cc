75b79a0a3000240bd717eeb2a6fdf0225b865983
Zkh-dot <kernzahar@gmail.com>
2024-09-12 20:32:18 +0300

    is authed func

--- src/auth.cc
+++ src/auth.cc
@@ -77,14 +77,21 @@ void auth(std::shared_ptr<httplib::Server> svr_ptr, cp::connection_pool pool) {
 
 bool is_authed(std::string token, cp::connection_pool& pool) { 
     auto decoded = jwt::decode(token);
+    std::string user;
     if (decoded.has_id())
-        auto user = decoded.get_id();
+        user = decoded.get_id();
     else return false;
 
-    // if user in db 
+    // if user in db true, else false 
+    cp::query get_user("SELECT userId User WHEREE userName=($1);");
+    auto tx = cp::tx(pool, get_user);
+    
+    pqxx::result result = get_user(user);
+
+    if(result.empty()) {
+        return false;
+    }
     return true;
-    // else
-    return false;
 }
 
 bool is_authed(const httplib::Request& req, cp::connection_pool& pool) {
@@ -92,20 +99,20 @@ bool is_authed(const httplib::Request& req, cp::connection_pool& pool) {
     return is_authed(token, pool);
 }
 
-int main() {
-    auto token = jwt::create()
-        .set_type("JWS")
-        .set_issuer("auth0")
-        .set_id("scv")
-        .set_payload_claim("user", jwt::claim(std::string("scv")))
-        .sign(jwt::algorithm::hs256{"secret"});
+// int main() {
+//     auto token = jwt::create()
+//         .set_type("JWS")
+//         .set_issuer("auth0")
+//         .set_id("scv")
+//         .set_payload_claim("user", jwt::claim(std::string("scv")))
+//         .sign(jwt::algorithm::hs256{"secret"});
 
-    std::cout<<token<<std::endl;
+//     std::cout<<token<<std::endl;
 
-    auto decoded = jwt::decode(token);  
+//     auto decoded = jwt::decode(token);  
 
-    for(auto& e : decoded.get_payload_json())
-        std::cout << e.first << " = " << e.second << std::endl;
+//     for(auto& e : decoded.get_payload_json())
+//         std::cout << e.first << " = " << e.second << std::endl;
 
-    std::cout<<std::endl;   
-}
+//     std::cout<<std::endl;   
+// }
