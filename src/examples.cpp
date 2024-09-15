5763c6e769b8e6f6dcade3a09a7d4a089294b8a0
Zkh-dot <kernzahar@gmail.com>
2024-09-15 11:00:39 +0300

    add logger to project

--- src/examples.cpp
+++ src/examples.cpp
@@ -8,6 +8,7 @@
 #include <pqxx/pqxx>
 #include "pqxx_cp.cc"
 #include "rapidjson/document.h" 
+#include "spdlog/spdlog.h"
 
 
 std::ifstream in("image.png", std::ios::in | std::ios::binary);
@@ -71,8 +72,6 @@ void connection_pool() {
 
     auto tx = cp::tx(pool, create_table);
 
-    tx.get().esc("");
-
     pqxx::result r = create_table();
 
     tx.commit();
@@ -82,7 +81,30 @@ void connection_pool() {
     std::cout<<'\n'<<s1<<'\n';
 } 
 
+void loger() 
+{
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
+    
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:<30}", "left aligned");
+    
+    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+    spdlog::debug("This message should be displayed..");    
+    
+    // change log pattern
+    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+    
+    // Compile time log levels
+    // Note that this does not change the current log level, it will only
+    // remove (depending on SPDLOG_ACTIVE_LEVEL) the call on the release code.
+    SPDLOG_TRACE("Some trace message with param {}", 42);
+    SPDLOG_DEBUG("Some debug message");
+}
+
 
 int main() {
-
 }
