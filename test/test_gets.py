87b492bcd9c9d4116df67e1fa52c93568d3c2100
Zkh-dot <kernzahar@gmail.com>
2025-02-10 18:48:13 +0300

    add tests for new functions

--- test/test_gets.py
+++ test/test_gets.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+import os
 
 BASE_URL = "http://localhost/api"
 TOKEN = "5261aa7439b988c0f93d38f676e3bfd2a070ddd64bf174282f37cfa3348320e9"
@@ -7,6 +8,14 @@ TOKEN = "5261aa7439b988c0f93d38f676e3bfd2a070ddd64bf174282f37cfa3348320e9"
 def test_get_achievements_by_user():
     username = "scv-7"
     response = requests.get(f"{BASE_URL}/achivements/user/{username}", verify=False)
+    assert response.status_code == 200
+    data = response.json()
+    assert "result" in data
+    assert isinstance(data["result"], list)
+
+def test_get_full_achievements_by_user():
+    username = "scv-7"
+    response = requests.get(f"{BASE_URL}/achivements/user/full/{username}", verify=False)
     assert response.status_code == 200
     data = response.json()
     assert "result" in data
@@ -27,6 +36,16 @@ def test_get_achievement_info():
     data = response.json()
     assert "result" in data
     assert isinstance(data["result"], list)
+
+def test_get_achievement_images():
+    response = requests.get(f"{BASE_URL}/achivements/pictures", verify=False)
+    assert response.status_code == 200
+    data = response.json()
+    assert "result" in data
+    assert isinstance(data["result"], list)
+    for picture in data["result"]:
+        assert os.path.exists(f"src/pages{picture}")
+
 
 def test_get_post_by_id():
     post_id = "1"
@@ -83,3 +102,12 @@ def test_admin_status():
     data = response.json()
     assert "status" in data
     assert isinstance(data["status"], str)
+
+def test_user_avatars():
+    response = requests.get(f"{BASE_URL}/user/all_avatars", verify=False)
+    assert response.status_code == 200
+    data = response.json()
+    assert "result" in data
+    assert isinstance(data["result"], list)
+    for avatar in data["result"]:
+        assert os.path.exists(f"src/pages{avatar}")
